8e6a78f97383e6f46b176d76d7014c1e61c67399
lurisdefacer <smuhammet210@gmail.com>
2024-04-25 20:39:04 +0300

    GithubApi

---
+++ GithubApi.py
@@ -0,0 +1,135 @@
+import requests
+import json
+from os import system, name
+
+print_header = \
+"""
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XX                                                                          XX
+XX   MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMMssssssssssssssssssssssssssMMMMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMss'''                          '''ssMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMyy''                                    ''yyMMMMMMMMMMMM   XX
+XX   MMMMMMMMyy''                                            ''yyMMMMMMMM   XX
+XX   MMMMMy''                                                    ''yMMMMM   XX
+XX   MMMy'                                                          'yMMM   XX
+XX   Mh'                                                              'hM   XX
+XX   -                                                                  -   XX
+XX                                                                          XX
+XX   ::                                                                ::   XX
+XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
+XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
+XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
+XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
+XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
+XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
+XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
+XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
+XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
+XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
+XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
+XX                                oo      oo                                XX
+XX           oM                 oo          oo                 Mo           XX
+XX         oMMo                M              M                oMMo         XX
+XX       +MMMM                 s              s                 MMMM+       XX
+XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
+XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
+XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
+XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
+XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
+XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
+XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
+XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
+XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
+XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
+XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
+XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
+XX   MMMMMMMM-                                                  -MMMMMMMM   XX
+XX   MMMMMMMMM                                                  MMMMMMMMM   XX
+XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
+XX   MMMMMMMMMMy.                                            .yMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMy.                                        .yMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMss.           ....           .ssMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
+XX                                                                          XX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ _                _       _____        __                    
+| |              (_)     |  __ \      / _|                   
+| |    _   _ _ __ _ ___  | |  | | ___| |_ __ _  ___ ___ _ __ 
+| |   | | | | '__| / __| | |  | |/ _ \  _/ _` |/ __/ _ \ '__|
+| |___| |_| | |  | \__ \ | |__| |  __/ || (_| | (_|  __/ |   
+|______\__,_|_|  |_|___/ |_____/ \___|_| \__,_|\___\___|_|  
+
+"""
+clear = lambda: system("cls")
+clear()
+print(print_header)
+
+def nmap_hello():
+        print("""
+
+  ------------------------------      
+  Github Api
+
+  github : lurisdefacer 
+  ------------------------------
+    """)
+
+nmap_hello()
+
+class Github:
+  def __init__(self):
+      self.api_url = 'https://api.github.com'
+      self.token = '' # Bu Kısma Sizin hesap Tokeniniz gelmesi gerekiyor.
+
+  def getUser(self, username):
+      response = requests.get(self.api_url+'/users/'+ username)
+      return  response.json()
+
+
+  def getRepositories(self, username):
+    response = requests.get(self.api_url+'/users/'+ username+'/repos')
+    return response.json()
+
+
+  def createRepository(self, name):
+    response = requests.post(self.api_url+'/user/repos?access_token='+ self.token, json={  
+        "name": name,
+        "description": "This is your first repository",
+        "homepage": "https://github.com",
+        "private": False,
+        "has_issues": True,
+        "has_projects": True,
+        "has_wiki": True
+    })
+    return response.json()
+
+github =  Github()
+
+
+
+while True:
+  secim = input('1- Find User\n2- Get Repositories\n3 - Create Repository\n4- Exit\nSeçim:');
+
+  if secim == '4':
+    break
+  else:
+      if secim == '1':
+        username = input('Username: ')
+        result = github.getUser(username)
+        print(f"name : {result['name']} public repos : {result['public_repos']} follower : {result['followers']}")
+      elif secim == '2':
+        username = input('Username: ')
+        result = github.getRepositories(username)
+        for repo in result:
+            print(repo['name'])
+      elif secim == '3':
+          name = input('repository name: ')
+          result = github.createRepository(name)
+          print(result)
+      else:
+          print("Yanlış Seçim")
